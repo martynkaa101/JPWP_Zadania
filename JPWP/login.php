24fdb5d856ef3806c1a6bb319856b2dc605f3606
pawlusmartyna <44200233+martynkaa101@users.noreply.github.com>
2020-05-15 17:01:52 +0200

    Update login.php

--- JPWP/login.php
+++ JPWP/login.php
@@ -7,11 +7,13 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $conn=mysqli_connect($servername, $mysql_user, $mysql_pass, $dbname);
     $email=$_POST['email'];
     $password=$_POST['password'];
-    $query="SELECT `name` FROM `registration` WHERE email='$email' AND password='$password'";
-    $result=mysqli_query($conn, $query);
-    $row=mysqli_fetch_array($result, MYSQLI_ASSOC);
-    if($row){
-    echo 'Hello ' . $row['name'];
+    $query="SELECT `name` FROM `registration` WHERE( email=? AND password=?)";
+    $stmt=mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, 'ss', $email, $password);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $name);
+    if(mysqli_stmt_fetch($stmt)){
+    echo 'Hello ' . $name;
     }else{
     echo("login failed");
     }
@@ -19,4 +21,3 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 } else {
     echo("error in request method");
 }
-?>
